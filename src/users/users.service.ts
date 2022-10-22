6d663b2abc4eed63edce174690bfd6228a36ebfb
YangGwangSeong <rhkdtjd_12@naver.com>
2022-10-22 07:23:06 -0700

    feat : create users service , email service

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import * as uuid from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { EmailService } from 'src/email/email.service';
+
+@Injectable()
+export class UsersService {
+    constructor(private emailService: EmailService){}
+    
+    async createUser(name: string, email: string, password: string) {
+        await this.checkUserExists(email);
+
+        const signupVerifyToken = uuid.v1();
+
+        await this.saveUser(name, email, password, signupVerifyToken);
+        await this.sendMemberJoinEmail(email, signupVerifyToken);
+    }
+
+    private checkUserExists(email: string) {
+        return false;
+    }
+
+    private saveUser(name: string, email: string, password: string, signupVerifyToken: string) {
+        return;
+    }
+
+    private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+        //await this.emailService.sendMemberJoinVerification(email, signupVerifyToken);
+        return;
+    }
+}
