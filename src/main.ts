22d710834528e2d8791da32725846bf81020f3c1
YangGwangSeong <rhkdtjd_12@naver.com>
2022-11-23 06:44:17 -0800

    feat : create user pipe validator

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from "dotenv";
 import * as path from "path";
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config({
   path: path.resolve(
@@ -12,6 +13,9 @@ dotenv.config({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform : true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,17 @@
+import { IsString, MinLength, MaxLength, IsEmail, Matches } from 'class-validator';
+
 export class CreateUserDto {
-    readonly name : string;
+    @IsString()
+    @MinLength(2)
+    @MaxLength(30)
+    readonly name: string;
+
+    @IsString()
+    @IsEmail()
+    @MaxLength(60)
     readonly email: string;
+    
+    @IsString()
+    @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
     readonly password: string;
 }
